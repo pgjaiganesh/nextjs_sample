b32169b7d0f8c6ccb20fd4eb42c491a80fc3b416
Jaiganesh <jaiganesh.girinathan@gmail.com>
2021-02-18 20:47:25 +0400

    updates

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
         <h1 className="title">
           Read{' '}
           <Link href="/posts/first-post">
-            <a>this page!</a>
+            <a>this page Om!</a>
           </Link>
         </h1>
 
